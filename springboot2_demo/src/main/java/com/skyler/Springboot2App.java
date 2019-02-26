a8bc8eb4f637993dc898e6bcb33d46ec8dcaf62b
yaoyuanyy <yaoliang_yl_ok@163.com>
2019-02-26 18:26:26 +0800

    add spring boot2

--- springboot2_demo/src/main/java/com/skyler/Springboot2App.java
+++ springboot2_demo/src/main/java/com/skyler/Springboot2App.java
@@ -1,4 +1,4 @@
-package com.s.skyler;
+package com.skyler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,13 +11,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *   NB.
  * </pre>
  * <p>
- * Created by skyler on 2019-02-25 at 18:54
+ * Created by skyler on 2019-02-26 at 18:20
  */
 @SpringBootApplication
-public class GatewayApp {
+public class Springboot2App {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApp.class, args);
-        // new SpringApplicationBuilder(GatewayApp.class).web(WebApplicationType.REACTIVE).run(args);
+        SpringApplication.run(Springboot2App.class, args);
     }
 }
---
+++ springcloud2_demo_gateway/src/main/java/com/skyler/config/RouteMappingConfiguration.java
@@ -0,0 +1,32 @@
+package com.skyler.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Description: 此处的配置与在application.yml中的配置相同
+ * <p></p>
+ * <pre>
+ *  application.yml file
+ *
+ *  spring:
+ *   cloud:
+ *     gateway:
+ *      routes:
+ *        - id: service1
+ *          uri: lb://service1
+ *          predicates:
+ *            - Path=/api-a/**
+ * </pre>
+ * Created by skyler on 2019-02-25 at 20:41
+ */
+@Configuration
+public class RouteMappingConfiguration {
+
+//    @Bean
+//    public RouteLocator routeLocator(RouteLocatorBuilder builder){
+//        return builder.routes().route(r -> r.path("/api-a/**").uri("lb://service1")).build();
+//    }
+}
